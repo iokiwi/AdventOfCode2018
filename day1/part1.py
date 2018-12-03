bbfb459f58d446e8999b0e9aea976b19ac451e40
Simon Merrick <s.m3rrick@gmail.com>
2018-12-03 15:31:48 +1300

    Replace tabs with spaces

--- day1/part1.py
+++ day1/part1.py
@@ -1,15 +1,15 @@
 def main():	
-	i = input()
-	values = []
-	while(i):
-		try: 
-			values.append(i)
-			i = input()
-		except EOFError:
-			break
+    i = input()
+    values = []
+    while(i):
+        try: 
+            values.append(i)
+            i = input()
+        except EOFError:
+            break
 
-	answer = reduce(lambda x, y: x+y, values)
-	print(answer)
+    answer = reduce(lambda x, y: x+y, values)
+    print(answer)
 
 if __name__ == "__main__":
 	main()
--- day1/part2.py
+++ day1/part2.py
@@ -1,49 +1,50 @@
 def read_input():
-	i = input()
-	values = []
-	while(i):
-		try: 
-			values.append(int(i))
-			i = input()
-		except EOFError:
-			break
-	return values
+    i = input()
+    values = []
+    while(i):
+        try: 
+            values.append(int(i))
+            i = input()
+        except EOFError:
+            break
+    return values
 
 
 def iterate_values(values, frequency_history=None, start_frequency=0):
-	""" Return the first recurring frequency
-	>>> iterate_values([1, -1])
-	0
-	>>> iterate_values([3, 3, 4, -2, -4])
-	10
-	>>> iterate_values([-6, 3, 8, 5, -6])
-	5
-	>>> iterate_values([7,7,-2,-7,-4])
-	14
-	"""
+    """ Return the first recurring frequency
+    >>> iterate_values([1, -1])
+    0
+    >>> iterate_values([3, 3, 4, -2, -4])
+    10
+    >>> iterate_values([-6, 3, 8, 5, -6])
+    5
+    >>> iterate_values([7,7,-2,-7,-4])
+    14
+    """
 
-	frequency = start_frequency
+    frequency = start_frequency
 
-	if frequency_history is None:
-		frequency_history = set()
+    if frequency_history is None:
+        frequency_history = set()
 
-	#print("{:<10}{:<10}{:<10}{}".format("f", "v", "f+v", "history"))
+    #print("{:<10}{:<10}{:<10}{}".format("f", "v", "f+v", "history"))
 
-	for value in values:
-		frequency_history.add(frequency)
-		#print("{:<10}{:<10}{:<10}{}".format(frequency, value, frequency + value, frequency_history))
-		frequency += value
-		if frequency in frequency_history:
-			return frequency
-	return iterate_values(values, frequency_history, frequency)
+    for value in values:
+        frequency_history.add(frequency)
+        #print("{:<10}{:<10}{:<10}{}".format(frequency, value, frequency + value, frequency_history))
+        frequency += value
+        if frequency in frequency_history:
+            return frequency
+    return iterate_values(values, frequency_history, frequency)
 
 
 def main():	
-	values = read_input()
-	answer = iterate_values(values)
-	print(answer)
-	
+    values = read_input()
+    answer = iterate_values(values)
+    print(answer)
+
+
 if __name__ == "__main__":
-	import doctest
-	doctest.testmod()
-	main()
+    import doctest
+    doctest.testmod()
+    main()
