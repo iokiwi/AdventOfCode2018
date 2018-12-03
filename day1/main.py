c24baaaaa29e5551d8b2341c47186754616fd724
Simon Merrick <s.m3rrick@gmail.com>
2018-12-03 13:50:09 +1300

    Day 1

---
+++ day1/main.py
@@ -0,0 +1,15 @@
+def main():	
+	i = input()
+	values = []
+	while(i):
+		try: 
+			values.append(i)
+			i = input()
+		except EOFError:
+			break
+
+	answer = reduce(lambda x, y: x+y, values)
+	print(answer)
+
+if __name__ == "__main__":
+	main()
